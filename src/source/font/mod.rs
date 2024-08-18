87629b83be59f9b7d9f8cc5ba0f10e19c83f431f
Thomas Reichert <thomasr1987@gmail.com>
2024-08-19 08:32:14 +1000

    Reorganise sound font module

---
+++ src/source/font/mod.rs
@@ -0,0 +1,77 @@
+mod range;
+
+use crate::{BufferConsumer, NoteConsumer, NoteEvent, NoteRange};
+use range::RangeData;
+
+const SOURCE_CAPACITY: usize = 8;
+
+pub struct SoundFontBuilder {
+    ranges: Vec<RangeData>,
+}
+
+impl SoundFontBuilder {
+    pub fn new() -> Self {
+        Self { ranges: vec![] }
+    }
+
+    pub fn add_range(
+        mut self,
+        range: NoteRange,
+        consumer_spawner: fn() -> Box<dyn BufferConsumer + Send + 'static>,
+    ) -> Self {
+        let mut consumers = Vec::new();
+        for _ in 0..SOURCE_CAPACITY {
+            consumers.push((0, consumer_spawner()));
+        }
+        self.ranges.push(RangeData {
+            range,
+            active_count: 0,
+            consumers,
+        });
+        self
+    }
+
+    pub fn build(self) -> SoundFont {
+        SoundFont::new(self.ranges)
+    }
+}
+
+pub struct SoundFont {
+    ranges: Vec<RangeData>,
+}
+
+impl SoundFont {
+    fn new(ranges: Vec<RangeData>) -> Self {
+        Self { ranges }
+    }
+}
+
+impl NoteConsumer for SoundFont {
+    fn restart_with_event(&mut self, event: &NoteEvent) {
+        let note: u8 = match event {
+            NoteEvent::NoteOn(note) => *note,
+            NoteEvent::NoteOff(note) => *note,
+        };
+        let turning_on = match event {
+            NoteEvent::NoteOn(_) => true,
+            NoteEvent::NoteOff(_) => false,
+        };
+        if turning_on {
+            for range_data in self.ranges.iter_mut() {
+                range_data.turn_note_on(note);
+            }
+        } else {
+            for range_data in self.ranges.iter_mut() {
+                range_data.turn_note_off(note);
+            }
+        }
+    }
+
+    fn fill_buffer(&mut self, buffer: &mut [f32]) {
+        for range_data in self.ranges.iter_mut() {
+            for i in 0..range_data.active_count {
+                range_data.consumers[i].1.fill_buffer(buffer);
+            }
+        }
+    }
+}
--- src/source/font/range.rs
+++ src/source/font/range.rs
@@ -1,8 +1,6 @@
-use crate::{BufferConsumer, NoteConsumer, NoteEvent, NoteRange};
+use crate::{BufferConsumer, NoteEvent, NoteRange};
 
-const SOURCE_CAPACITY: usize = 8;
-
-struct RangeData {
+pub struct RangeData {
     pub range: NoteRange,
     pub active_count: usize,
     pub consumers: Vec<(u8, Box<dyn BufferConsumer + Send + 'static>)>,
@@ -54,77 +52,6 @@ impl RangeData {
             removed_source.1.set_note(NoteEvent::NoteOff(note));
             self.consumers.push(removed_source);
             self.active_count -= 1;
-        }
-    }
-}
-
-pub struct SoundFontBuilder {
-    ranges: Vec<RangeData>,
-}
-
-impl SoundFontBuilder {
-    pub fn new() -> Self {
-        Self { ranges: vec![] }
-    }
-
-    pub fn add_range(
-        mut self,
-        range: NoteRange,
-        consumer_spawner: fn() -> Box<dyn BufferConsumer + Send + 'static>,
-    ) -> Self {
-        let mut consumers = Vec::new();
-        for _ in 0..SOURCE_CAPACITY {
-            consumers.push((0, consumer_spawner()));
-        }
-        self.ranges.push(RangeData {
-            range,
-            active_count: 0,
-            consumers,
-        });
-        self
-    }
-
-    pub fn build(self) -> SoundFont {
-        SoundFont::new(self.ranges)
-    }
-}
-
-pub struct SoundFont {
-    ranges: Vec<RangeData>,
-}
-
-impl SoundFont {
-    fn new(ranges: Vec<RangeData>) -> Self {
-        Self { ranges }
-    }
-}
-
-impl NoteConsumer for SoundFont {
-    fn restart_with_event(&mut self, event: &NoteEvent) {
-        let note: u8 = match event {
-            NoteEvent::NoteOn(note) => *note,
-            NoteEvent::NoteOff(note) => *note,
-        };
-        let turning_on = match event {
-            NoteEvent::NoteOn(_) => true,
-            NoteEvent::NoteOff(_) => false,
-        };
-        if turning_on {
-            for range_data in self.ranges.iter_mut() {
-                range_data.turn_note_on(note);
-            }
-        } else {
-            for range_data in self.ranges.iter_mut() {
-                range_data.turn_note_off(note);
-            }
-        }
-    }
-
-    fn fill_buffer(&mut self, buffer: &mut [f32]) {
-        for range_data in self.ranges.iter_mut() {
-            for i in 0..range_data.active_count {
-                range_data.consumers[i].1.fill_buffer(buffer);
-            }
         }
     }
 }
