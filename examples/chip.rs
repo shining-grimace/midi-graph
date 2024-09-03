70a5e950f6a37c0442fa92ad289445ac0b2de7e0
Thomas Reichert <thomasr1987@gmail.com>
2024-09-04 06:29:00 +1000

    Update chiptune example with noise channel in the current MIDI file

--- examples/chip.rs
+++ examples/chip.rs
@@ -2,18 +2,24 @@ extern crate midi_graph;
 
 use cpal::traits::StreamTrait;
 use midi_graph::{
-    util::smf_from_file, BaseMixer, MidiSourceBuilder, NoteRange, SoundFontBuilder,
-    SquareWaveSource, TriangleWaveSource,
+    util::smf_from_file, BaseMixer, LfsrNoiseSource, MidiSourceBuilder, NoteRange,
+    SoundFontBuilder, SquareWaveSource, TriangleWaveSource,
 };
 use std::time::Duration;
 
 const MIDI_FILE: &'static str = "resources/sample-in-c.mid";
 
-const SQUARE_CHANNEL: usize = 1;
 const TRIANGLE_CHANNEL: usize = 0;
+const SQUARE_CHANNEL: usize = 1;
+const NOISE_CHANNEL: usize = 2;
 
 fn main() {
     let smf = smf_from_file(MIDI_FILE).unwrap();
+    let triangle_font = SoundFontBuilder::new()
+        .add_range(NoteRange::new_inclusive_range(0, 255), || {
+            Box::new(TriangleWaveSource::new(1.0))
+        })
+        .build();
     let square_font = SoundFontBuilder::new()
         .add_range(NoteRange::new_inclusive_range(0, 50), || {
             Box::new(SquareWaveSource::new(0.125, 0.5))
@@ -22,14 +28,15 @@ fn main() {
             Box::new(SquareWaveSource::new(0.125, 0.875))
         })
         .build();
-    let triangle_font = SoundFontBuilder::new()
+    let noise_font = SoundFontBuilder::new()
         .add_range(NoteRange::new_inclusive_range(0, 255), || {
-            Box::new(TriangleWaveSource::new(1.0))
+            Box::new(LfsrNoiseSource::new(0.25, false, 50))
         })
         .build();
     let midi = MidiSourceBuilder::new(smf)
-        .add_channel_font(SQUARE_CHANNEL, square_font)
         .add_channel_font(TRIANGLE_CHANNEL, triangle_font)
+        .add_channel_font(SQUARE_CHANNEL, square_font)
+        .add_channel_font(NOISE_CHANNEL, noise_font)
         .build()
         .unwrap();
     let mixer = BaseMixer::from_source(Box::new(midi));
