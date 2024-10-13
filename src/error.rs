a93bc2554dcce69c1414302c9d8adff5d17d8748
Thomas Reichert <thomasr1987@gmail.com>
2024-10-14 06:34:54 +1100

    Implement Display for Error

--- src/error.rs
+++ src/error.rs
@@ -10,6 +10,21 @@ pub enum Error {
     NoDevice,
 }
 
+impl std::fmt::Display for Error {
+    fn fmt(&self, fmt: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::User(e) => e.fmt(fmt),
+            Error::Io(e) => e.fmt(fmt),
+            Error::Ron(e) => e.fmt(fmt),
+            Error::Midly(e) => e.fmt(fmt),
+            Error::Hound(e) => e.fmt(fmt),
+            Error::Soundfont(e) => fmt.write_fmt(format_args!("{:?}", e)),
+            Error::Cpal(e) => e.fmt(fmt),
+            Error::NoDevice => "No audio device available".fmt(fmt),
+        }
+    }
+}
+
 impl From<std::io::Error> for Error {
     fn from(value: std::io::Error) -> Self {
         Error::Io(value)
