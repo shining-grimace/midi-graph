2b90c0a8e10e7d14a79fb7c3742b45f78637b0fe
Thomas Reichert <thomasr1987@gmail.com>
2025-04-09 06:23:59 +1000

    Fix old syntax in wasm_demo

--- src/wasm_demo.rs
+++ src/wasm_demo.rs
@@ -1,9 +1,8 @@
-use crate::{util::midi_builder_from_bytes, BaseMixer, MidiSource, SquareWaveSource};
-use cpal::traits::StreamTrait;
+use crate::{util::midi_builder_from_bytes, BaseMixer, SquareWaveSource};
 use std::time::Duration;
 use wasm_bindgen::prelude::*;
 
-const MIDI_FILE: &'static [u8] = include_bytes!("../resources/dansenapolitaine.mid");
+const MIDI_FILE: &'static [u8] = include_bytes!("../resources/LoopingMidi.mid");
 
 #[wasm_bindgen]
 pub fn play_stream() {
@@ -12,9 +11,6 @@ pub fn play_stream() {
         .add_channel_source(0, Box::new(SquareWaveSource::new(None, 0.25, 0.125)))
         .build()
         .unwrap();
-    let mixer = BaseMixer::from_source(midi_source);
-    let stream = mixer.open_stream().expect("Could not open stream");
-    stream.play().expect("Could not play the stream");
+    let _mixer = BaseMixer::start_single_program(Box::new(midi_source));
     std::thread::sleep(Duration::from_secs(5));
-    stream.pause().expect("Could not pause the stream");
 }
